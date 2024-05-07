09ffaa01ddb478f392bd57f5725efbcd38b232b0
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-07 11:32:36 +0100

    JavaScript

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Sammyalade/repo.git
-git push -u origin main
+//By Id
+const wrapper = document.getElementById("wrapper")
+console.log(wrapper)
+
+//By className
+const title = document.getElementsByClassName("title")
+console.log(title)
+console.log(Array.isArray(Array.from("title")));
+
+//By TagName
+const head = document.getElementsByTagName("head")
+console.log(head)
+
+//By QuerySelector
+const bookList = document.querySelectorAll("#book-list ul li")
+console.log(bookList)
+
+bookList.forEach(book => {
+    console.log(book.textContent)
+})
+
+console.log(bookList[1].textContent)
